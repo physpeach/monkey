d687915fba73d835df5a2d025b0032a428d7aefb
PhysPeach <Masato.Shukawa@outlook.jp>
2022-02-14 23:10:00 +0900

    テストをパスするようにNextTokenを実装

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "monkey/token"
+
 type Lexer struct {
 	input        string
 	position     int  //current index of input string
@@ -21,4 +23,37 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var ret token.Token
+	switch l.ch {
+	case '=':
+		ret = newToken(token.ASSIGN, l.ch)
+	case ';':
+		ret = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		ret = newToken(token.LPAREN, l.ch)
+	case ')':
+		ret = newToken(token.RPAREN, l.ch)
+	case ',':
+		ret = newToken(token.COMMA, l.ch)
+	case '+':
+		ret = newToken(token.PLUS, l.ch)
+	case '{':
+		ret = newToken(token.LBRACE, l.ch)
+	case '}':
+		ret = newToken(token.RBRACE, l.ch)
+	case 0:
+		ret.Literal = ""
+		ret.Type = token.EOF
+	}
+
+	l.readChar()
+
+	return ret
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
