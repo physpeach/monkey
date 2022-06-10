5bf8cb2570dc1b5bae3cd9fcdb9ed4d65d827579
PhysPeach <Masato.Shukawa@outlook.jp>
2022-06-10 09:16:16 +0900

    中間演算子の構文解析のテストを記述。

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -195,6 +195,54 @@ func TestParsingPrefixExpressions(t *testing.T) {
 	}
 }
 
+func TestParsingInfixExpressions(t *testing.T) {
+	prefixTests := []struct {
+		input    string
+		leftVal  int64
+		operator string
+		rightVal int64
+	}{
+		{"5 + 5", 5, "+", 5},
+		{"5- 5", 5, "-", 5},
+		{"5 *5", 5, "*", 5},
+		{"5 / 5", 5, "/", 5},
+		{"7 > 10", 7, ">", 10},
+		{"7 < 10", 7, "<", 10},
+		{"5 == 5", 5, "==", 5},
+		{"5 != 5", 5, "!=", 5},
+	}
+
+	for _, tt := range prefixTests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain %d statments. got=%d\n", 1, len(program.Statements))
+		}
+
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T", program.Statements[0])
+		}
+
+		exp, ok := stmt.Expression.(*ast.InfixExpression)
+		if !ok {
+			t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
+		}
+		if !testIntegerLiteral(t, exp.Left, tt.leftVal) {
+			return
+		}
+		if exp.Operator != tt.operator {
+			t.Fatalf("exp.Operator is not '%s'. got='%s'", tt.operator, exp.Operator)
+		}
+		if !testIntegerLiteral(t, exp.Right, tt.rightVal) {
+			return
+		}
+	}
+}
+
 func testIntegerLiteral(t *testing.T, il ast.Expression, val int64) bool {
 	integer, ok := il.(*ast.IntegerLiteral)
 	if !ok {
