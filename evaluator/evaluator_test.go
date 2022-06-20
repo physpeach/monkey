d7c7be0ca654438790c032c2e44dd0f2311da4bd
PhysPeach <Masato.Shukawa@outlook.jp>
2022-06-21 08:58:33 +0900

    テストtypo

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -114,10 +114,9 @@ func TestFunctionCall(t *testing.T) {
 	}{
 		{"let identity = fn(x){x;}; identity(5);", 5},
 		{"let identity = fn(x){return x}; identity(5);", 5},
-		{"let double = fn(x) { x + 2; }; double(5);", 10},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
 		{"let add = fn(x, y) {x + y;}; add(5, 5);", 10},
-		{"let add = fn(x, y) {x + y;}; add(5+5, add(5,5);", 20},
-		{"let add = fn(x, y) {x + y;}; add(5+5, add(5+5));", 20},
+		{"let add = fn(x, y) {x + y;}; add(5+5, add(5,5));", 20},
 		{"fn(x){x;}(5)", 5},
 	}
 
