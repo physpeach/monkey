5dddd99d159cfa76d87dcfdfd01695cd9e51eddf
PhysPeach <Masato.Shukawa@outlook.jp>
2022-06-20 21:00:27 +0900

    ユーザ体験を向上させるパーサーエラー関数にした

--- repl/repl.go
+++ repl/repl.go
@@ -9,6 +9,18 @@ import (
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -37,6 +49,9 @@ func Start(in io.Reader, out io.Writer) {
 
 func printParserErrors(out io.Writer, errors []string) {
 	for _, msg := range errors {
+		io.WriteString(out, MONKEY_FACE)
+		io.WriteString(out, "Woops! We ran into some monkey business here! \n")
+		io.WriteString(out, " parser errors:\n")
 		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
