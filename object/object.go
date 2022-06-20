e18e77fa9b16675d94898ea17db03a5523ac1f82
PhysPeach <Masato.Shukawa@outlook.jp>
2022-06-20 21:31:45 +0900

    Null オブジェクトを追加

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ type ObjectType string
 const (
 	BOOLEAN_OBJ = "BOOLEAN"
 	INTEGER_OBJ = "INTEGER"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -29,3 +30,8 @@ type Integer struct {
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
